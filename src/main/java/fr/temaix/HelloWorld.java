674f1401d220670176571300d875c3f19d5938dd
yann <ymagnabosco@hopps-group.com>
2017-04-20 19:08:50 +0200

    Hello world

--- src/main/java/fr/temaix/HelloWorld.java
+++ src/main/java/fr/temaix/HelloWorld.java
@@ -6,6 +6,6 @@ package fr.temaix;
 public class HelloWorld {
 
     public static void main(String[] args) {
-        System.out.println("Hello World.");
+        System.out.println("Hello World");
     }
 }
