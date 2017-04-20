dfa84d3beec193a8a4659cfafa6c6eb9a368099c
yann <ymagnabosco@hopps-group.com>
2017-04-20 18:55:32 +0200

    Hello world

---
+++ src/main/java/fr/temaix/HelloWorld.java
@@ -0,0 +1,11 @@
+package fr.temaix;
+
+/**
+ * Created by yann on 20/04/2017.
+ */
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
